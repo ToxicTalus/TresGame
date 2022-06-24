55b44b0aa0f96f0bac1867aef597b32698ffaa38
Narknon <narknon0@gmail.com>
2022-06-23 21:53:52 -0400

    start

--- Source/KineDriver/KineDriverRt/Public/SQEX_KineDriverData.h
+++ Source/KineDriver/KineDriverRt/Public/SQEX_KineDriverData.h
@@ -24,7 +24,7 @@
 
 class USkeleton;
 
-UCLASS()
+UCLASS(Blueprintable)
 class KINEDRIVERRT_API USQEX_KineDriverData : public UObject {
     GENERATED_BODY()
 public:
--- Source/TresGameEditor/TresGameEditor.Build.cs
+++ Source/TresGameEditor/TresGameEditor.Build.cs
@@ -9,7 +9,6 @@ public class TresGameEditor : ModuleRules {
             "AnimGraphRuntime",
             "AnimationCore",
             "BonamikRt",
-            "BonamikEd",
             "Core",
             "CoreUObject",
             "CriWareRuntime",
