29c76eb8e24941421cb0824eece261b1a0633cbb
Narknon <narknon0@gmail.com>
2022-06-24 19:33:33 -0400

    add standard assets

--- Plugins/EnginePlugins/SQEX/SQEXSEAD/Source/SQEXSEADEd/Public/SQEXSEADEd.h
+++ Plugins/EnginePlugins/SQEX/SQEXSEAD/Source/SQEXSEADEd/Public/SQEXSEADEd.h
@@ -1,12 +1,13 @@
 // Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
-
 #pragma once
 #include "CoreMinimal.h"
 #include "UObject/Object.h"
 #include "SQEXSEADEd.generated.h"
 
+UCLASS()
 class USQEXSEADEd : public UObject
 {
+	GENERATED_BODY()
 public:
-
+	USQEXSEADEd();
 };
