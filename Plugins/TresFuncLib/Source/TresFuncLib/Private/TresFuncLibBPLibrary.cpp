e811568312abb64b189c1e4a8e161c31a3d85308
Narknon <narknon0@gmail.com>
2022-06-24 22:48:59 -0400

    updates

---
+++ Plugins/TresFuncLib/Source/TresFuncLib/Private/TresFuncLibBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "TresFuncLibBPLibrary.h"
+#include "TresFuncLib.h"
+
+UTresFuncLibBPLibrary::UTresFuncLibBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UTresFuncLibBPLibrary::TresFuncLibSampleFunction(float Param)
+{
+	return -1;
+}
+
